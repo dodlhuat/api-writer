4eead913dcd13dd6c0d924263a03d6e588b42754
Andi Bauer <abauer@asqs.net>
2023-09-25 15:50:27 +0200

    wip api.php

---
+++ api.php
@@ -0,0 +1,21 @@
+<?php
+// TODO: defining endpoints
+// TODO: variables in endpoints
+// TODO: only allow GET and POST
+// TODO: token authentication
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+
+echo json_encode([
+  'method' => $requestMethod,
+  'uri' => $uri
+]);
